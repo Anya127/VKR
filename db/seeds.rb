2a3b166814b7f439f5ab308f2044793fbb425c46
lety <lety@localhost.localdomain>
2018-03-10 21:33:34 +0300

    10_03_2018

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,14 +4,14 @@ a = ["Иванов","Петров","Смирнов","Кузнецов","Попо
 b = ["Николай","Дмитрий","Константин","Ярослав","Павел","Михаил","Евгений","Александр","Петр","Алексей"].shuffle
 d = ["Александрович","Евгеньевич","Егорович","Аркадьевич","Робертович","Данилович","Леонидович","Кириллович","Ярославович","Юрьевич"].shuffle
 for i in 0 ... n
+  c = Category.create(name_c: "name_c" + i.to_s, full_name_c: "full_name_c" + i.to_s, d_vocation_c: "vocation_days" + i.to_s)
+  s = Post.create(name_p: "name_p" + i.to_s, full_name_p: "full_name_p" + i.to_s, category_id: c.id)
+  u = Unit.create(name_u: "name_u" + i.to_s, full_name_u: "full_name_u" + i.to_s)
+  r = Relation.create(d_vocation_pu: "vocation_days" + i.to_s, post_id: s.id, unit_id: u.id)
   w = Worker.create(l_name: a[i].to_s, f_name: b[i].to_s, s_name: d[i].to_s, inn: "inn12345678" + i.to_s, passport: "passp1234" + i.to_s, snils: "snils12345" + i.to_s, d_bday: Date.today )
-  Contract.create(number_c: "number" + i.to_s , d_conclusion_c: Date.today, d_expiration_c: Date.today , stavka: "1." + i.to_s  , vocation_c: "vocation_days" + i.to_s  , special_conditions: "spec" + i.to_s, worker_id: w.id)
+  t = Contract.create(number_c: "number" + i.to_s , d_conclusion_c: Date.today, d_expiration_c: Date.today , stavka: "1." + i.to_s  , vocation_c: "vocation_days" + i.to_s  , special_conditions: "spec" + i.to_s, worker_id: w.id, relation_id: r.id)
+  v = Vocation.create(type_v: "1" + i.to_s, d_conclusion_v: Date.today, d_expiration_v: Date.today, is_real: "true", order_date: Date.today, order_number: "order_number " + i.to_s, contract_id: t.id)
 
-  Category.create(name_c: "name_c" + i.to_s, full_name_c: "full_name_c" + i.to_s, d_vocation_c: "vocation_days" + i.to_s)
-  Unit.create(name_u: "name_u" + i.to_s, full_name_u: "full_name_u" + i.to_s)
-  Post.create(name_p: "name_p" + i.to_s, full_name_p: "full_name_p" + i.to_s)
-  Relation.create(d_vocation_pu: "vocation_days" + i.to_s)
-  Vocation.create(type_v: "1" + i.to_s, d_conclusion_v: Date.today, d_expiration_v: Date.today, is_real: "true", order_date: Date.today, order_number: "order_number " + i.to_s)
 end
 
 if (u1 = User.find_by_email('admin@localhost')).nil?
